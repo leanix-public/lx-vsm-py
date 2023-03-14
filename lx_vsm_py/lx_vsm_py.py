bdcc5cd8a80e05ed4256235a215fc92d1425a680
Donovon Simpson <donovon.simpson@leanix.net>
2023-03-14 08:32:43 -0500

    parameterize a few inputs

--- lx_vsm_py/lx_vsm_py.py
+++ lx_vsm_py/lx_vsm_py.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import requests
 
@@ -19,18 +20,18 @@ def get_access_token(api_token, host):
     return response.json()["access_token"]
 
 
-def upload_file(file_path, region, access_token):
+def upload_service(file_path, region, access_token, name, description):
     """
-    Upload the given file to the LeanIX VSM Service Discovery API.
+    Upload the service to the LeanIX VSM Service Discovery API.
     """
     url = f"https://{region}-vsm.leanix.net/services/vsm/discovery/v1/service"
     headers = {"Authorization": f"Bearer {access_token}"}
     data = {
-        "id": "test_id",
+        "id": name,
         "sourceType": "python3",
         "sourceInstance": "lx-vsm-python",
-        "name": "test_name",
-        "description": "test_description",
+        "name": name,
+        "description": description,
     }
     with open(file_path, "rb") as f:
         files = {"bom": f}
@@ -41,13 +42,17 @@ def upload_file(file_path, region, access_token):
 def main():
   parser = argparse.ArgumentParser(description='Upload CycloneDX files to LeanIX VSM Service Discovery API')
 
-  parser.add_argument('--file', help='The path to the CycloneDX file to upload')
-  parser.add_argument('--api_token', help='The API token to use for authentication')
-  parser.add_argument('--region', help='The region to use for uploading the file (e.g., "us" or "eu")')
-  parser.add_argument('--host', help='The host to use for authenticating (e.g., acme.leanix.net would be "acme")')
+  parser.add_argument('--file', help='The path to the CycloneDX file to upload.')
+  parser.add_argument('--api_token', help='The API token to use for authentication.')
+  parser.add_argument('--region', help='The region to use for uploading the file (e.g., "us" or "eu").')
+  parser.add_argument('--host', help='The host to use for authenticating (e.g., acme.leanix.net would be "acme").')
+  parser.add_argument('--name', help='The name of this project/service. Defaults to the current folder name.', required=False)
+  parser.add_argument('--description', help='The description of this project/service.', default='')
   args = parser.parse_args()
 
   access_token = get_access_token(args.api_token, args.host)
   if access_token:
-      upload_file(args.file, args.region, access_token)
+      # Use the name from the arg if it was passed in, or get the current folder name otherwise
+      name = args.name if args.name is not None else os.path.basename(os.getcwd()) 
+      upload_service(args.file, args.region, access_token, name, args.description)
       print("Complete!")
