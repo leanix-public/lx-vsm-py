f5f56c4f4f37caece61e085976d9e42a91cb2140
Donovon Simpson <donovon.simpson@leanix.net>
2023-03-17 15:55:54 -0500

    update README and add support for additional data

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # LX-VSM-PY
 
-## How to use this package
+## What is it? 
+
+This is a python based CLI to submit project information, including a [CycloneDX](https://cyclonedx.org/) compliant SBOM, to your [LeanIX VSM](https://www.leanix.net/en/products/value-stream-management) workspace via the [Service Discovery API](https://docs-vsm.leanix.net/reference/discovery_service).
+
+## When should I use it?
+
+Primarily used for submitting an SBOM during your CI/CD pipeline, but can be used to submit additional data during various stages of development and deployment, such as version numbers, testing results, code quality, and deployment times.
+
+## How does it work?
+
+Assuming your python packages are on your system `$PATH` variable, this can be run like any other bash tool just by calling `lx-vsm-py`. Arguments can be seen by running `lx-vsm-py --help`. This script uses the arguments to authenticate with your workspace to submit the other arguments and (optional) SBOM to the [Service Discovery API](https://docs-vsm.leanix.net/reference/discovery_service). See the next section for detailed instructions.
+
+## How do I use it?
 
 1. Clone this repository to your computer
 2. Run `python setup.py sdist && pip install dist/lx-vsm-py-1.0.tar.gz` to build and install this utility to your local python installation globally.
@@ -10,3 +22,11 @@ cyclonedx-py -e -F --format json -o sbom.json &&  lx-vsm-py --file sbom.json --a
 ```
 4. Update the region, host, and api-token for your instance of VSM
 5. Run `./lx-vsm-py.sh` to update your service in VSM
+
+## License
+
+This project is licensed under the MIT License
+
+## Contact
+
+Start with the [VSM Documentation](https://docs-vsm.leanix.net/docs), or feel free to contact [LeanIX Support](https://leanix.zendesk.com/hc/en-us/community/topics) for anything else.
--- lx_vsm_py/lx_vsm_py.py
+++ lx_vsm_py/lx_vsm_py.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import requests
+import json
 
 def get_access_token(api_token, host):
     """
@@ -20,39 +21,49 @@ def get_access_token(api_token, host):
     return response.json()["access_token"]
 
 
-def upload_service(file_path, region, access_token, name, description):
+def upload_service(file_path, region, access_token, name, description, data):
     """
     Upload the service to the LeanIX VSM Service Discovery API.
     """
     url = f"https://{region}-vsm.leanix.net/services/vsm/discovery/v1/service"
     headers = {"Authorization": f"Bearer {access_token}"}
-    data = {
+    body = {
         "id": name,
         "sourceType": "python3",
         "sourceInstance": "lx-vsm-python",
         "name": name,
         "description": description,
+        "data": data
     }
-    with open(file_path, "rb") as f:
-        files = {"bom": f}
-        response = requests.post(url, headers=headers, data=data, files=files)
+    if file_path == '':
+        response = requests.post(url, headers=headers, data=body)
+    else:
+        with open(file_path, "rb") as f:
+            files = {"bom": f}
+            response = requests.post(url, headers=headers, data=body, files=files)
     if response.status_code != 200:
         raise ValueError("Failed to upload file.")
     
 def main():
   parser = argparse.ArgumentParser(description='Upload CycloneDX files to LeanIX VSM Service Discovery API')
 
-  parser.add_argument('--file', help='The path to the CycloneDX file to upload.')
-  parser.add_argument('--api_token', help='The API token to use for authentication.')
+  parser.add_argument('--sbom-path', help='The path to the CycloneDX file to upload. Supports both XML or JSON. Defaults to "./sbom.json"', default='./sbom.json')
+  parser.add_argument('--api-token', help='The API token to use for authentication.')
   parser.add_argument('--region', help='The region to use for uploading the file (e.g., "us" or "eu").')
   parser.add_argument('--host', help='The host to use for authenticating (e.g., acme.leanix.net would be "acme").')
   parser.add_argument('--name', help='The name of this project/service. Defaults to the current folder name.', required=False)
   parser.add_argument('--description', help='The description of this project/service.', default='')
+  parser.add_argument('--data', help='Optional metadata in a simple {"key":"value"} json format.', default='{}')
   args = parser.parse_args()
 
   access_token = get_access_token(args.api_token, args.host)
+
   if access_token:
       # Use the name from the arg if it was passed in, or get the current folder name otherwise
       name = args.name if args.name is not None else os.path.basename(os.getcwd()) 
-      upload_service(args.file, args.region, access_token, name, args.description)
+      # Make sure the SBOM exists, skip otherwise
+      sbom_path = args.sbom_path if os.path.exists(args.sbom_path) else ''
+
+      upload_service(sbom_path, args.region, access_token, name, args.description, args.data)
+
       print("Complete!")
